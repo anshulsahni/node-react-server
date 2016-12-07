9a7814bced4a4731285ea0d4afafba1435040936
Anshul Sahni <anshul_sahni@live.com>
2016-12-07 02:19:06 -0800

    wrapping up logger to display date and only prints when in development

--- src/logger.js
+++ src/logger.js
@@ -1,3 +1,9 @@
-module.exports = function(message) {
-  console.log(message);
+var moment = require('moment');
+
+module.exports = function(environment) {
+  return function(message) {
+    if (environment.toLowerCase() === 'development') {
+      console.log(moment().format('YYYY-MM-DD HH:mm:ss - ') + message);
+    }
+  }
 };
