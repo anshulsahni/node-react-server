65c4a69a7b89cf279ee61920503649884063d27b
Anshul Sahni <anshul_sahni@live.com>
2016-12-07 02:31:36 -0800

    removed moment from the project

--- src/logger.js
+++ src/logger.js
@@ -1,9 +1,17 @@
-var moment = require('moment');
+
+const pad = function(val) {
+  return (String(val).length == 1 ? '0' : '') + val;
+}
+
+const timestamp = function() {
+  const date = new Date();
+  return date.getFullYear() + '-' + pad((date.getMonth() + 1)) + '-' + pad(date.getDate()) + ' ' + pad(date.getHours()) + ':' + pad(date.getMinutes()) + ':' + pad(date.getSeconds());
+}
 
 module.exports = function(environment) {
   return function(message) {
     if (environment.toLowerCase() === 'development') {
-      console.log(moment().format('YYYY-MM-DD HH:mm:ss - ') + message);
+      console.log(timestamp() + ' - ' + message);
     }
   }
 };
