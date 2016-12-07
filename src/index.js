ab56811511e9420be433ad537a215278e2ab1abd
Anshul Sahni <anshul_sahni@live.com>
2016-12-07 02:04:52 -0800

    added comments and method to handle if specified port is not available

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+const portfinder = require('portfinder');
+
 const log = require('./logger');
 
 module.exports = function(options) {
@@ -9,6 +11,7 @@ module.exports = function(options) {
   const RENDER_FILE = options.renderFile || 'index.html';
   const server = express();
 
+  // method to handle all the routes and re-direct to single render-file
   server.get('*', function(request, response) {
     response.sendFile(path.resolve(process.cwd(), APP_DIR, RENDER_FILE), function(error) {
       if (error.status == 404) {
@@ -18,7 +21,18 @@ module.exports = function(options) {
     });
   });
 
-  server.listen(PORT, function() {
-    log('Server started at 127.0.0.1:' + PORT);
+  // method to check if port is already in use
+  portfinder.basePort = PORT;
+  portfinder.getPort(function(error, assignedPort) {
+    console.log('ansh');
+    if (assignedPort !== PORT) {
+      log('ERROR: Specified port ('+ PORT +') is already in use');
+      log('ADVICE: Next available port is ' + assignedPort + ' please retry the server with same');
+      process.exit(1);
+    } else {
+      server.listen(PORT, function() {
+        log('Server started at 127.0.0.1:' + PORT);
+      });
+    }
   });
 }
