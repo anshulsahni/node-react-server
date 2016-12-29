369700ecd6dc82913395c1313b089fdff8523585
Anshul Sahni <anshul_sahni@live.com>
2016-12-30 00:29:16 +0530

    using express to render application

--- src/index.js
+++ src/index.js
@@ -2,24 +2,30 @@ const express = require('express');
 const path = require('path');
 const portfinder = require('portfinder');
 
-
 module.exports = function(options) {
   const ENV = options.env || 'development';
   const PORT = options.port || (ENV === 'development' ? 3000 : 8080);
   const APP_DIR = options.appDir || 'app';
   const RENDER_FILE = options.renderFile || 'index.html';
   const server = express();
+  const app = express();
   const log = require('./logger')(ENV);
 
   // method to handle all the routes and re-direct to single render-file
-  server.get('*', function(request, response) {
+  app.get('/', function(request, response) {
     response.sendFile(path.resolve(process.cwd(), APP_DIR, RENDER_FILE), function(error) {
-      if (error.status == 404) {
+      if (error && error.status == 404) {
         log('ERROR: Render file specified does not exist in the specified App directory');
         response.status(404).send('App is in still development mode');
       }
     });
   });
+
+  // setting static files path
+  server.use('/__assets', express.static(path.resolve(process.cwd(), APP_DIR)));
+
+  // setting to render app on all routes
+  server.use('*', app);
 
   // method to check if port is already in use
   portfinder.basePort = PORT;
