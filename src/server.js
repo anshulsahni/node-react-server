ca8136531d3a3eda2830745f32160ab1d7befca8
Anshul Sahni <anshul_sahni@live.com>
2017-04-12 11:40:06 +0530

    index.js is just made to import server

---
+++ src/server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const path = require('path');
+const portfinder = require('portfinder');
+
+module.exports = function(options) {
+  const PORT = options.port || 8080;
+  const APP_DIR = options.appDir || 'app';
+  const RENDER_FILE = options.renderFile || 'index.html';
+  const LOG_FILE = options.logFile || 'react-server.log';
+  const server = express();
+  const app = express();
+  const log = require('./logger')(LOG_FILE);
+
+  // method to handle all the routes and re-direct to single render-file
+  app.get('/', function(request, response) {
+    response.sendFile(path.resolve(process.cwd(), APP_DIR, RENDER_FILE), function(error) {
+      if (error && error.status == 404) {
+        log('ERROR: Render file specified does not exist in the specified App directory');
+        response.status(404).send('App is in still development mode');
+      } else {
+        log('SUCCESS: Rendered ' + request.originalUrl + ' from host: ' + request.hostname + ':' + PORT);
+      }
+    });
+  });
+
+  // setting static files path
+  server.use('/__assets', express.static(path.resolve(process.cwd(), APP_DIR)));
+
+  // setting to render app on all routes
+  server.use('*', app);
+
+  // method to check if port is already in use
+  portfinder.basePort = PORT;
+  portfinder.getPort(function(error, assignedPort) {
+    if (assignedPort !== PORT) {
+      log('ERROR: Specified port ('+ PORT +') is already in use');
+      log('ADVICE: Next available port is ' + assignedPort + ' please retry the server with same');
+      process.exit(1);
+    } else {
+      server.listen(PORT, function() {
+        log('Server started at 127.0.0.1:' + PORT);
+      });
+    }
+  });
+}
