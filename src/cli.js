10232739dcf7075e258ced18e51f97cda2aba9da
Anshul Sahni <anshul_sahni@live.com>
2017-04-12 20:24:07 +0530

    added description for every parameter

--- src/cli.js
+++ src/cli.js
@@ -4,9 +4,16 @@ const server = require('./server.js');
 const argv = yargs
               .usage('Usage: $0 <options>')
               .alias('p', 'port')
+              .describe('p', 'Port on which server should be started and files are served')
               .alias('r', 'render-file')
+              .describe('r', '.html file which will be sent as response by the server')
               .alias('a', 'app-dir')
+              .describe('a', 'Directory name in from in which file render file resides - relative to cwd()')
               .alias('l', 'log-file')
+              .describe('l', 'Log file in which logs will be recored')
+              .help('h')
+              .alias('h', 'help')
+              .describe('h', 'Show help related to react-server')
               .argv;
 
 server({
