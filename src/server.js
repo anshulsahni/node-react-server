1dc50107803048ceb29d95c1288b85bcbffa43d7
Anshul Sahni <anshul_sahni@live.com>
2017-04-17 00:52:57 +0530

    code refactoring

--- src/server.js
+++ src/server.js
@@ -11,43 +11,43 @@ const portfinder = require('portfinder');
  */
 
 module.exports = function(options) {
-  const PORT = options.port || 8080;
-  const APP_DIR = options.appDir || 'app';
-  const RENDER_FILE = options.renderFile || 'index.html';
-  const LOG_FILE = options.logFile || 'react-server.log';
+  const port = options.port || 8080;
+  const appDir = options.appDir || 'app';
+  const renderFile = options.renderFile || 'index.html';
+  const logFile = options.logFile || 'react-server.log';
   const logLevel = options.logLevel || false;
   const server = express();
   const app = express();
-  const log = require('./logger')(logLevel, LOG_FILE);
+  const log = require('./logger')(logLevel, logFile);
 
   // method to handle all the routes and re-direct to single render-file
   app.get('/', function(request, response) {
-    response.sendFile(path.resolve(process.cwd(), APP_DIR, RENDER_FILE), function(error) {
+    response.sendFile(path.resolve(process.cwd(), appDir, renderFile), function(error) {
       if (error && error.status == 404) {
         log('ERROR: Render file specified does not exist in the specified App directory');
         response.status(404).send('App is in still development mode');
       } else {
-        log('SUCCESS: Rendered ' + request.originalUrl + ' from host: ' + request.hostname + ':' + PORT);
+        log('SUCCESS: Rendered ' + request.originalUrl + ' from host: ' + request.hostname + ':' + port);
       }
     });
   });
 
   // setting static files path
-  server.use('/__assets', express.static(path.resolve(process.cwd(), APP_DIR)));
+  server.use('/__assets', express.static(path.resolve(process.cwd(), appDir)));
 
   // setting to render app on all routes
   server.use('*', app);
 
   // method to check if port is already in use
-  portfinder.basePort = PORT;
+  portfinder.basePort = port;
   portfinder.getPort(function(error, assignedPort) {
-    if (assignedPort !== PORT) {
-      log('ERROR: Specified port ('+ PORT +') is already in use');
+    if (assignedPort !== port) {
+      log('ERROR: Specified port ('+ port +') is already in use');
       log('ADVICE: Next available port is ' + assignedPort + ' please retry the server with same');
       process.exit(1);
     } else {
-      server.listen(PORT, function() {
-        log('Server started at 127.0.0.1:' + PORT);
+      server.listen(port, function() {
+        log('Server started at 127.0.0.1:' + port);
       });
     }
   });
